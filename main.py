11781d5200286937da0015e3c406dec4f231041b
Alex Devlin <alexwdevlin@gmail.com>
2023-08-11 14:00:30 -0700

    Searches all files in the desired folder and finds all files named "Ultrasound Depth and Focus Not Found.csv". It then reads these files and saves the depth and focus values into a new CSV file stored in the root folder for this project.

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import os
+import csv
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def check_cal_data(walk_dir):
+    csv_title = "Ultrasound Depth and Focus Not Found.csv"
+    os.remove("Calibration Files to Take.csv")
+    for (root, dirs, files) in os.walk(walk_dir):
+        if csv_title in files:
+            find_missing_cal(os.path.join(root, csv_title))
+            print(root)
 
 
-# Press the green button in the gutter to run the script.
+def find_missing_cal(file_path):
+    with open(file_path, newline='') as csvfile:
+        cal_reader = csv.reader(csvfile, quotechar='|')
+        for row in cal_reader:
+            if "Scan name" not in row:
+                write_missing_cal(row[1:])
+
+
+def write_missing_cal(new_cal_line):
+    with open('Calibration Files to Take.csv', 'a', newline='') as csvfile:
+        writier = csv.writer(csvfile, delimiter=",")
+        writier.writerow(new_cal_line)
+
+
 if __name__ == '__main__':
-    print_hi('STIMULUS')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    check_cal_data(r"S:\13. STIMULUS DATA\STIM001")
